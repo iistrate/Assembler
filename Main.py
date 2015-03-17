40a7a7a7fa95b219682ec151105a83525c00dbd0
Ioan George Istrate <istrate_ioan_george@yahoo.com>
2015-03-17 10:29:26 -0400

    added better error checking when loading files, and re tested the program

--- Main.py
+++ Main.py
@@ -7,24 +7,22 @@ def main():
     askForFile = True;
     while (askForFile):
         try:
-            #fileName = input("Please enter file name to read: ")
-            fileName = "Pong.asm"
+            fileName = input("Please enter file name to read: ")
             asmFile = open(fileName, 'r')
             askForFile = False
+            FileParser = Parser.Parser(asmFile)
+            #close file
+            asmFile.close()
+
+            #list of commands to be interpreted
+            rawCommands = FileParser.getContents
+
+            HackAssembler = Assembler.Assembler(rawCommands, fileName)
+            HackAssembler.addUserSymbols()
+            HackAssembler.translate()
+            HackAssembler.outputFile()
         except:
             print("Error, no such file")
-    
-        FileParser = Parser.Parser(asmFile)
-        #close file
-        asmFile.close()
-
-        #list of commands to be interpreted
-        rawCommands = FileParser.getContents
-
-        HackAssembler = Assembler.Assembler(rawCommands, fileName)
-        HackAssembler.addUserSymbols()
-        HackAssembler.translate()
-        HackAssembler.outputFile()
 
 if __name__ == '__main__': 
     main()
